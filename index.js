21f97e73aa60a825339dbba2dc449abcf2e5e90b
Ariel Pellegrino <ariel.pell@gmail.com>
2021-03-03 21:48:52 -0300

    tester

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.get('/:usuario', (req, res) => {
-    console.log(req.params);
-    return res.send('Hello Word' + usuario);
+const listaUsuarios = [
+    'Ivens',
+    'Jailson',
+    'Maria',
+    'Natalia',
+    'Ariel',
+    'Nykolle'
+]
+
+// Query Params
+// Ex: localhost:3000/usuarios?nome=Ivens
+app.get('/usuarios', (req, res) => {
+    const { nome } = req.query;
+    let listaRetorno = listaUsuarios.filter(i => i.includes(nome || ''));
+    return res.json(listaRetorno);
 });
 
-//app.get('/usuarios/:id', (req, res))
+// Route Params 
+// Ex: ecommer.com.br/produtos/4/detalhes
+app.get('/hello/:usuario', (req, res) => {    
+    const { usuario } = req.params;
+    return res.send(`Hello world ${usuario}`);
+});
 
-app.listen(1233, () => {
-    console.log('Servidor rodando na porta 1234');
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
 });
