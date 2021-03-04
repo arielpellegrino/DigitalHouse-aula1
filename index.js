86f771a22fee376bd2ad503ae88daa83b7c5e092
Ariel Pellegrino <ariel.pell@gmail.com>
2021-03-03 21:03:10 -0300

    update3

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express();
 app.get('/', (req, res) => {
     return res.send('Hello Word');
 });
-app.listen(1234, () => {
+app.listen(1233, () => {
     console.log('Servidor rodando na porta 1234');
 });
