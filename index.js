fab75baa38c7194112265fc9721fed1b30873679
Ariel Pellegrino <ariel.pell@gmail.com>
2021-03-10 21:38:59 -0300

    next update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const axios = require('axios');
-const app = express();
+const path = require('path');
 
+const app = express();
 const api = axios.create({
     baseURL: 'https://pokeapi.co/api/v2/'
 });
@@ -12,45 +13,43 @@ const listaUsuarios = [
     'Maria',
     'Natalia',
     'Ariel',
-    'Nykolle',
+    'Nykolle'
 ]
+
+app.use(express.static(path.join(__dirname, '/src/assets/')));
 
 // Query Params
 // Ex: localhost:3000/usuarios?nome=Ivens
 app.get('/usuarios', (req, res) => {
-    const {
-        nome
-    } = req.query;
+    const { nome } = req.query;
     let listaRetorno = listaUsuarios.filter(i => i.includes(nome || ''));
     return res.json(listaRetorno);
 });
 
 // Route Params 
 // Ex: ecommer.com.br/produtos/4/detalhes
-app.get('/hello/:usuario', (req, res) => {
-    const {
-        usuario
-    } = req.params;
+app.get('/hello/:usuario', (req, res) => {    
+    const { usuario } = req.params;
     return res.send(`Hello world ${usuario}`);
 });
 
 app.get('/pokemons', async (req, res) => {
     const responseAxios = await api.get('pokemon?limit=100&offset=200');
-    const {
-        data
-    } = responseAxios;
+    const { data } = responseAxios;
     const retorno = {
         ...data,
         results: data.results.map(item => {
-            item.greetings = 'Olá';
+            item.greetings = 'Olá';            
             return item;
         })
     }
     return res.json(retorno);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/index.html'));
+});
+
 app.listen(1234, () => {
     console.log('Servidor rodando na porta 1234');
 });
-
-//
